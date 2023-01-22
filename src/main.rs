78ffc199ae2fdfffb3e80279f4df5cde9adc541a
mattygersh21 <dakotaseia@hotmail.com>
2023-01-22 17:45:15 -0600

    * Included positional argument intake on command line. * Included flag/optional argument intake on command line. * Added Config struct. * Updated run command to take a Config argument.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = rust_catr::run() {
+    if let Err(e) = rust_catr::get_args().and_then(rust_catr::run) {
         eprintln!("{}",e);
         std::process::exit(1);
     }
