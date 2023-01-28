6a6ec090e638cf26db425b09d31cfe884e1ae3c1
mattygersh21 <dakotaseia@hotmail.com>
2023-01-28 15:07:59 -0600

    * Updated run method to read lines of given file(s) with a specified numbering format, if included as an flag.

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,27 @@ type MyResult<T> = Result<T,Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
-        println!("{}",filename);
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}",filename,err),
+            Ok(file) => {
+                let mut last_num = 0;
+                for (line_num,line) in file.lines().enumerate() {
+                    let line = line?;
+                    if config.number_lines {
+                        println!("{:>6}\t{}",line_num+1,line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            last_num += 1;
+                            println!("{:>6}\t{}",last_num,line);
+                        } else {
+                            println!("{}",line);
+                        }
+                    } else {
+                        println!("{}",line);
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
